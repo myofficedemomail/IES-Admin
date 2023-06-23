10907eee5ef4f82ef0761a150e4403499cd8bc74
Soumya <myofficedemomail@gmail.com>
2023-06-23 18:02:55 +0530

    sad

---
+++ IES-Admin/src/main/java/com/ashokit/config/SwaggerConfig.java
@@ -0,0 +1,78 @@
+package com.ashokit.config;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+//@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+
+	@Bean
+	public Docket apiDoc() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.ashokit.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("MyApp Rest APIs",
+                "APIs for MyApp.",
+                "1.0",
+                "Terms of service",
+                new Contact("test", "www.org.com", "test@emaildomain.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey(AUTHORIZATION_HEADER, "JWT", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference(AUTHORIZATION_HEADER, authorizationScopes));
+    }
+
+}
