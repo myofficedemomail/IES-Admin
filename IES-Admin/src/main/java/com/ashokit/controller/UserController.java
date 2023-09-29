d6c5d46a490728bff0150ae91f2fa2b875b6c8bd
Soumya <myofficedemomail@gmail.com>
2023-09-29 16:03:54 +0530

    Added for logined user

--- IES-Admin/src/main/java/com/ashokit/controller/UserController.java
+++ IES-Admin/src/main/java/com/ashokit/controller/UserController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +30,10 @@ public class UserController {
 
 	@PostMapping("createAccount")
 	public ResponseEntity<UserDto> createAccountForCaseWorker(@Valid @RequestBody UserDto userDto) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		System.out.println("**************************"+currentPrincipalName);
+		System.out.println(authentication.getDetails().toString());
 		UserDto createdCaseWorkerAccount = userService.createUser(userDto);
 		return new ResponseEntity<>(createdCaseWorkerAccount, HttpStatus.CREATED);
 	}
